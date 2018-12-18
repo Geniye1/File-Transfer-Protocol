531aebb4550d24988d93aaf84d57239ea61a979b
Jack Strayer <jstrayer57@gmail.com>
2018-12-17 21:59:57 -0500

    Initial commit: Basic ability to send messengers between client and server

---
+++ Client/Client_Form.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Diagnostics;
+
+namespace Client
+{
+    public partial class Client_Form : Form
+    {
+        public enum DataTransferType {
+            Text = 0,
+            Image = 1
+        }
+
+        public Client_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void Client_Form_Load(object sender, EventArgs e)
+        {
+            int o = (int)DataTransferType.Text;
+            
+        }
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // IP and port the TCP server is running on
+                string ip = "192.168.1.9";
+                const int port = 9999;
+
+                // Initialize a new TcpClient
+                TcpClient client = new TcpClient();
+
+                // Update the user
+                OutputDialog.Text += "Attempting connection with " + ip + " and port " + port + "\n" +
+                                     "---------------------------------------------------------------\n";
+                
+                // Connect the client to the given IP and port
+                client.Connect("192.168.1.9", 9999);
+
+                // Update the user
+                OutputDialog.Text += "Connection successully established...\n" +                                    
+                                     "---------------------------------------------------------------\n";
+
+                // Get the NetworkStream from the connected client
+                NetworkStream stream = client.GetStream();
+
+                // Encoder to encode the message
+                ASCIIEncoding asc = new ASCIIEncoding();
+
+                // Populate a byte array with the encoded message
+                byte[] data = asc.GetBytes(MessageDialog.Text);
+
+                // Update the user
+                OutputDialog.Text += "Transmitting message...\n" +
+                                     "---------------------------------------------------------------\n";
+
+                // Write the message to the stream and the TCP server
+                stream.Write(data, 0, data.Length);
+
+                // Prepare and receive a response to the server
+                byte[] response = new byte[100];
+                int length = stream.Read(response, 0, 100);
+
+                // Push the response to the user
+                for (int i = 0; i < length; i++)
+                {
+                    OutputDialog.Text += Convert.ToChar(response[i]);
+                }
+
+                OutputDialog.Text += "\n---------------------------------------------------------------\n";
+
+                // Clean up
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.StackTrace);
+            }
+        }
+    }
+}
---
+++ TCP_Messenger/Server_Form.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+/*
+ * Features still-to-be-made:
+ * --------------------------------------------------
+ * Send and save images
+ * Send and save files (code, text files)
+ * Be able to choose if you want the application to
+ * host a TCP server or be a TCP client
+*/ 
+
+namespace TCP_Messenger
+{
+    public partial class Server_Form : Form
+    {
+
+        private bool hasBegun = false;
+        private TcpListener listener;
+        private Socket s;
+
+        public Server_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void Server_Form_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BeginButton_Click(object sender, EventArgs e)
+        {
+            // Turn off the button
+            BeginButton.Enabled = false;
+
+            // Begin the recursive server algorithm
+            RunServer();
+
+            hasBegun = true;
+        }
+
+        async private void RunServer()
+        {
+            try
+            {
+                if (!hasBegun)
+                {
+                    // Define the IP and port of the TCP server
+                    IPAddress ip = IPAddress.Parse("192.168.1.9");
+                    const int port = 9999;
+
+                    // Intialize a listener to listen on the given IP and port
+                    listener = new TcpListener(ip, port);
+
+                    // Update the user
+                    OutputDialog.Text += "Server begun and listening on port " + port + "\n" +
+                                          "Local endpoint is " + listener.LocalEndpoint + "\n" +
+                                          "Awaiting a connection...\n" +
+                                          "--------------------------------------------------------------\n";
+                    // Begin listening for connections
+                    listener.Start();
+                }
+
+                // Push a new thread to run the given delegate asynchronously to prevent UI hang
+                await Task.Run(() =>
+                {
+                    // Accept the connection's socket
+                    s = listener.AcceptSocket();            
+                    
+                    // Update user (Invoke is used as two threads cannot be updating the same variable at once
+                    this.Invoke(new MethodInvoker(delegate ()
+                    {
+                        OutputDialog.Text += "Connection established from " + s.RemoteEndPoint + "\n" +
+                                         "Attempting to read data...\n" +
+                                         "---------------------------------------------------------------\n";
+                    }));
+
+                    // Initialize a buffer for the data and receive the data
+                    byte[] buffer = new byte[100];
+                    int length = s.Receive(buffer);
+
+                    // Update the user
+                    this.Invoke(new MethodInvoker(delegate ()
+                    {
+                        OutputDialog.Text += "Data received...\n" +
+                                         "---------------------------------------------------------------\n";
+                    }));
+                    
+                    // Push the message to the Output dialog box
+                    this.Invoke(new MethodInvoker(delegate ()
+                    {
+                        for (int i = 0; i < length; i++)
+                        {
+                            OutputDialog.Text += Convert.ToChar(buffer[i]);
+                        }
+
+                        OutputDialog.Text += "\n---------------------------------------------------------------";
+                    }));
+
+                    // Encoder to send response to client
+                    ASCIIEncoding asc = new ASCIIEncoding();
+
+                    // Send the response
+                    s.Send(asc.GetBytes("This was changed from the server-side code!"));
+
+                    // Update the user
+                    this.Invoke(new MethodInvoker(delegate ()
+                    {
+                        OutputDialog.Text += "\nThe server successfully received the acknowledgement\n" +
+                                        "---------------------------------------------------------------\n";
+                    }));
+
+                    // Clean up 
+                    s.Close();
+
+                    // Recursively run the server again
+                    RunServer();
+                });
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.StackTrace);
+            }
+
+        }
+    }
+}
